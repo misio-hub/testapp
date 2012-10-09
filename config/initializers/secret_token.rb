3028318eb2a43247c078ca1d2efea69a8bdeb5f6
Łukasz Probierz <misio@misio-HP-550.(none)>
2012-10-09 03:05:30 +0200

    utworzenie aplikacji

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'bed6c6464422a533c71ae80054949b17a90bc80c28690fc9562b2a76f3f9f31b04d05e18551fd38465f540e67c650ac0a82f9e6868c60c126d6ddc0e50f5a40b'
